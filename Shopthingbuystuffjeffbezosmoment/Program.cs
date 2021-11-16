44c01246cc55a71d5ed50bc5f0488080298a2194
Castor Dunkels <castordnk@gmail.com>
2021-11-16 14:56:14 +0100

    början

---
+++ Shopthingbuystuffjeffbezosmoment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+int jeffbezosallowance = 1000000000;
+
+Console.WriteLine($"you have {jeffbezosallowance} dollars\n");
+
+Console.WriteLine("Hello there Bezos-sama!!!!!!!!!!!!!!\nWhat would you like to do today?\n\nBuy another genital shaped rocket (1000000000)\nPay your workers(100000)\nEat lunch(2100)");
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
